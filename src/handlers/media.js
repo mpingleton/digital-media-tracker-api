d00fa2cc47effc05ec447240409ffa258e613004
Michael Pingleton <mnpingleton@gmail.com>
2022-07-31 20:03:15 +0300

    Added route which gets all media assigned to the currently authenticated user.

--- src/handlers/media.js
+++ src/handlers/media.js
@@ -1,10 +1,36 @@
 const mediaService = require('../services/media');
 const containerService = require('../services/container');
+const assignmentService = require('../services/assignments');
 
 const getMediaById = async (req, res) => {
     const media = await mediaService.getMediaById(req.params.mediaId);
 
     res.send(200, media);
+};
+
+const getMediaInMe = async (req, res) => {
+    const assignedFacilities = await assignmentService.getAssignmentsByUser(req.user.id);
+    
+    const containerPromises = [];
+    for (facilityId of assignedFacilities) {
+        containerPromises.push(containerService.getContainersInFacility(facilityId));
+    }
+    const containerArrays = await Promise.all(containerPromises);
+    const containers = containerArrays.flat(2);
+
+    const mediaPromises = [];
+    for (container of containers) {
+        mediaPromises.push(mediaService.getMediaInContainer(container.id));
+    }
+    const mediaArrays = await Promise.all(mediaPromises);
+    const media = mediaArrays.flat(2);
+
+    const data = {
+        numberMedia: media.length,
+        media: media,
+    };
+
+    res.send(200, data);
 };
 
 const getMediaInContainer = async (req, res) => {
@@ -40,6 +66,7 @@ const createMedia = async (req, res) => {
 
 module.exports = {
     getMediaById,
+    getMediaInMe,
     getMediaInContainer,
     getMediaInFacility,
     createMedia,
