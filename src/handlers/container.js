bfd07eecdcc601da9f3abfb1f2079d7025176072
Michael Pingleton <mnpingleton@gmail.com>
2022-08-03 20:18:50 +0300

    Added new route which gets all of the containers which a currently authenticated user has access to.

--- src/handlers/container.js
+++ src/handlers/container.js
@@ -1,8 +1,26 @@
 const containerService = require('../services/container');
+const assignmentService = require('../services/assignments');
 
 const getContainerById = async (req, res) => {
     const container = await containerService.getContainerById(req.params.containerId);
     res.send(200, container);
+};
+
+const getContainersInMe = async (req, res) => {
+    const assignedFacilities = await assignmentService.getAssignmentsByUser(req.user.id);
+
+    const containerPromises = [];
+    for (facilityId of assignedFacilities) {
+        containerPromises.push(containerService.getContainersInFacility(facilityId));
+    }
+    const containerArrays = await Promise.all(containerPromises);
+    const containers = containerArrays.flat(2);
+
+    const data = {
+        numberContainers: containers.length,
+        containers: containers,
+    };
+    res.send(200, data);
 };
 
 const getContainersInFacility = async (req, res) => {
@@ -21,6 +39,7 @@ const createContainer = async (req, res) => {
 
 module.exports = {
     getContainerById,
+    getContainersInMe,
     getContainersInFacility,
     createContainer,
 };
--- src/routes/container.js
+++ src/routes/container.js
@@ -8,6 +8,7 @@ const validator = require('../middleware/validator');
 const router = express.Router();
 
 router.get('/id/:containerId', sessionMiddleware.verifySessionAndRole(["ADMIN", "USER"]), validator(containerValidator.getContainerById), containerHandler.getContainerById);
+router.get('/in/me', sessionMiddleware.verifySessionAndRole(["ADMIN", "USER"]), containerHandler.getContainersInMe);
 router.get('/in/facility/:facilityId', sessionMiddleware.verifySessionAndRole(["ADMIN", "USER"]), validator(containerValidator.getContainersInFacility), containerHandler.getContainersInFacility);
 router.put('/', sessionMiddleware.verifySessionAndRole("ADMIN"), validator(containerValidator.createContainer), containerHandler.createContainer);
 
